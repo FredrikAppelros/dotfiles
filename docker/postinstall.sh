d4a70eac9d5a2f6d0fd9a858a9f1ed7daab8cf8a
Fredrik Appelros <fredrik.appelros@gmail.com>
2016-01-26 22:09:19 +0100

    ShellCheck'd
    You better check yo' self before you wreck yo' self!

--- docker/postinstall.sh
+++ docker/postinstall.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # Create the docker group and add myself
-sudo usermod -aG docker $(whoami)
+sudo usermod -aG docker "$(whoami)"
 
 # Start on boot
-if [ $(lsb_release -r | grep -oP "Release:\s\K\d+") -gt 14 ]; then
+if [ "$(lsb_release -r | grep -oP "Release:\s\K\d+")" -gt 14 ]; then
   sudo systemctl enable docker
 fi
