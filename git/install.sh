ccf53305182562c6a5c48c0514134f59386340c2
Fredrik Appelros <fredrik.appelros@proceranetworks.com>
2017-08-17 18:21:00 +0200

    Add diff-highlight
    Download, make and install diff-highlight from git/contrib.

--- git/install.sh
+++ git/install.sh
@@ -2,13 +2,27 @@
 
 TMPPATH=$(mktemp)
 
-# Get latest version
+# Download latest git archive
+wget -q -O $TMPPATH https://github.com/git/git/archive/master.zip
+
+# Extract git archive
+unzip $TMPPATH -d /tmp
+
+# Make diff-highlight
+make -C /tmp/git-master/contrib/diff-highlight
+
+# Install diff-highlight
+cp /tmp/git-master/contrib/diff-highlight/diff-highlight "$DOTFILESDIR/bin"
+
+TMPPATH=$(mktemp)
+
+# Get latest version of hub
 version=$(curl -s https://github.com/github/hub/releases/latest | grep -oP "(\d\.?)+")
 
-# Download archive
+# Download hub archive
 wget -q -O $TMPPATH "https://github.com/github/hub/releases/download/v$version/hub-linux-amd64-$version.tgz"
 
-# Extract archive
+# Extract hub archive
 tar xzf $TMPPATH -C /tmp
 
 # Install hub
