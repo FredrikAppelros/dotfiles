72c93b58743fed663c79bb0f28f23f4cb1b40b04
Fredrik Appelros <fredrik.appelros@proceranetworks.com>
2017-08-17 17:56:05 +0200

    Remove node postinstall step
    We can simply merge the postinstall actions into the install script.

--- node/install.sh
+++ node/install.sh
@@ -4,3 +4,13 @@ NVMDIR="$(readlink -e "$(dirname $0)/nvm.symlink")"
 
 # Checkout latest version
 cd $NVMDIR && git checkout "$(git describe --abbrev=0 --tags)"
+
+# Initialize nvm for the rest of this session
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+
+# Install latest node version
+nvm install node
+
+# Set node as default version
+nvm alias default node
