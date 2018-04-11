e4a2f72712d6a46c4bcbf5b8979d803caa30455b
Fredrik Appelros <fredrik.appelros@hypergene.se>
2018-04-11 16:18:17 +0200

    Update Spotify key

--- spotify/preinstall.sh
+++ spotify/preinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Add the Spotify repository signing key
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410
 
 # Add the Spotify repository
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
