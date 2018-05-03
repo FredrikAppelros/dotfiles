4b2e91e32205960c2bf846bdf2de44430bbc9ea7
Fredrik Appelros <fredrik.appelros@gmail.com>
2018-05-03 09:54:31 +0200

    Update docker installation

--- docker/postinstall.sh
+++ docker/postinstall.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 
-# Create the docker group and add myself
-sudo usermod -aG docker "$(whoami)"
+# Create the the current user to the docker group
+sudo usermod -aG docker "$USER"
 
 # Start on boot
 if [ "$(lsb_release -r | grep -oP "Release:\s\K\d+")" -gt 14 ]; then
   sudo systemctl enable docker
 fi
+
+# Configure local DNS servers
+nmcli d show enp0s31f6 | grep IP4.DNS | awk '{print $2}' \
+  | jq -sR '{dns: rtrimstr("\n")|split("\n")}' \
+  |  sudo tee /etc/docker/daemon.json > /dev/null
+
+# Restart the docker service
+sudo systemctl restart docker.service
 
 # Install docker-compose
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose && \
--- docker/preinstall.sh
+++ docker/preinstall.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
-codename=$(lsb_release -c | grep -oP "Codename:\s\K\w+")
-
-# Add the Docker repository signing key
-sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-
-# Add the Docker repository
-echo deb https://apt.dockerproject.org/repo ubuntu-$codename main | sudo tee /etc/apt/sources.list.d/docker.list
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
