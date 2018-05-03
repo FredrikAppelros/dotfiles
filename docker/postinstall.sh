80dd8a0b6ec8773ec27127503f75a3388f127257
Fredrik Appelros <fredrik.appelros@hypergene.se>
2018-05-03 09:18:32 +0200

    Install docker-compose

--- docker/postinstall.sh
+++ docker/postinstall.sh
@@ -7,3 +7,7 @@ sudo usermod -aG docker "$(whoami)"
 if [ "$(lsb_release -r | grep -oP "Release:\s\K\d+")" -gt 14 ]; then
   sudo systemctl enable docker
 fi
+
+# Install docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose && \
+    sudo chmod +x /usr/local/bin/docker-compose
